f42a69c8fd677b101bef8dd1ef96c68e8e9d50d4
nozar7 <rolin.espinozafelipe@gmail.com>
2022-01-26 08:14:51 -0500

    update code

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let numeroLikes = document.querySelector("#numLikes");
+let numeroLikes2 = document.querySelector("#numLikes2");
+let numeroLikes3 = document.querySelector("#numLikes3");
+
+function addLike() {
+    numeroLikes.value++;
+}
+
+function addLike2() {
+    numeroLikes2.value++;
+}
+
+function addLike3() {
+    numeroLikes3.value++;
+}
